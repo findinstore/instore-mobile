bc12852d217877cc755d988557ddf141fefe9d04
Chris Picato <christopherpicato@gmail.com>
2017-01-06 22:13:09 -0800

    Begin implementation of geolocation

--- components/Map.js
+++ components/Map.js
@@ -7,6 +7,54 @@ import {
 import MapView from 'react-native-maps';
 
 export class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialPosition: 'unknown',
+      lastPosition: 'unknown',
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421
+    }
+
+    this.setFirstLocation = this.setFirstLocation.bind(this);
+  }
+
+  watchID: ?number = null;
+
+  setFirstLocation() {
+    setTimeout(() => {
+      this.setState({
+        lastPostion: initialPosition
+      });
+    }, 10000);
+  }
+
+  componentWillMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        var initialPosition = JSON.stringify(position);
+        console.log(initialPosition, 'position');
+        this.setState({
+          initialPosition
+        });
+      },
+      (error) => alert(JSON.stringify(error)),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lastPosition = JSON.stringify(position);
+      this.setState({
+        lastPosition
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -18,12 +66,19 @@ export class Map extends Component {
             bottom: 0,
             left: 0
           }}
+          showsUserLocation={true}
           initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: this.state.latitudeDelta,
+            longitudeDelta: this.state.longitudeDelta
           }}
+          // region={{
+          //   latitude: this.state.lastPosition.latitude,
+          //   longitude: this.state.lastPosition.longitude,
+          //   latitudeDelta: this.state.lastPosition.latitudeDelta,
+          //   longitudeDelta: this.state.lastPosition.longitudeDelta
+          // }}
         />
       </View>
     )
