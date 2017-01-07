eb3e3307ae401d8ba46c7e176bf996e534afb7fb
Chris Picato <christopherpicato@gmail.com>
2017-01-06 20:37:00 -0800

    Create components folder Move map code to App file Add navbar

--- index.ios.js
+++ index.ios.js
@@ -5,55 +5,29 @@
  */
 
 import React, { Component } from 'react';
+import { App } from './components/App';
 import {
   AppRegistry,
   StyleSheet,
+  View,
   Text,
-  View
+  NavigatorIOS
 } from 'react-native';
-import MapView from 'react-native-maps';
 
 export default class instore extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <MapView
-          style={{
-            position: 'absolute',
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0
-          }}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
-      </View>
+      <NavigatorIOS
+        style={{
+          flex: 1
+        }}
+        initialRoute={{
+          component: App,
+          title: 'Instore'
+        }}
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('instore', () => instore);
